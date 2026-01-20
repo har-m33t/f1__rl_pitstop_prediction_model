a95a3586f44cf44c31c4710bff121e5cebd3be0f
Harmeet Singh <harmees2@uci.edu>
2026-01-20 00:17:26 -0800

    feat(load_data): implement load_event function

--- data/load_data.py
+++ data/load_data.py
@@ -1,7 +1,22 @@
 import fastf1
 import pandas as pd
 
-def load_event(event_name: str):
-    return
+def load_event(event_name: str, year: int = 2021) -> pd.DataFrame:
+    """
+    Docstring for load_event
+    
+    :param event_name: Event Name
+        example: "French Grand Prix" 
+    :type event_name: str
+    :param year: Year of Event
+    :type year: int
+    :return: event
+    :rtype: DataFrame
+
+    Load event data for a given race track and year using the FastF1 API. 
+    """
+    event = fastf1.get_event(year, event_name)
+    return event
+
 def load_lap_data(session: pd.DataFrame):
     return
