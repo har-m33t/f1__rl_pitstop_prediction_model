3b7c795569167a87cae351e3c87ede153a4e91f4
Harmeet Singh <harmees2@uci.edu>
2026-01-20 01:15:44 -0800

    feat(test_load_data.py): add skeleton for test functions in each class

--- tests/data_tests/test_load_data.py
+++ tests/data_tests/test_load_data.py
@@ -1,10 +1,32 @@
 import unittest
 
-class TestLoadEvent(unittest.TestCase): 
+class TestLoadEvent(unittest.TestCase):
+    def test_load_event_regular():
+        return
+    
+    def test_load_event_doesnt_exist():
+        return 
+    
+    def test_load_event_improper_type():
+        return
     pass
     
 class TestLoadSession(unittest.TestCase):
-    pass
+    def test_load_session_regular():
+        return
+    
+    def test_load_session_doesnt_exist():
+        return 
+    
+    def test_load_session_improper_type():
+        return
 
 class TestLoadLapData(unittest.TestCase):
-    pass
+    def test_load_lap_data_regular():
+        return
+    
+    def test_load_lap_data_session_doesnt_exist():
+        return 
+    
+    def test_load_session_improper_type():
+        return
