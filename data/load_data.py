8ad8bf7aac85eb9992a1e1e9f55a7b71b15aa93b
Harmeet Singh <harmees2@uci.edu>
2026-01-20 00:51:34 -0800

    feat(load_data): implement load_session function

--- data/load_data.py
+++ data/load_data.py
@@ -18,8 +18,19 @@ def load_event(event_name: str, year: int = 2021) -> pd.DataFrame:
     event = fastf1.get_event(year, event_name)
     return event
 
-def load_session(year: int = 2021, race_id: int = 1, *additional_info: str ):
-    return
+def load_session(year: int = 2021, race_id: int = 1, type: str = 'R' ):
+    """
+    Docstring for load_session
+    
+    :param year: Year of Event
+    :type year: int
+    :param race_id: Race Number in Race Season
+    :type race_id: int
+    :param type: Q-> Qualifiying, R-> Race, etc. 
+    :type type: str
+    """
+    session = fastf1.get_session(year, race_id, type)
+    return session
 
 def load_lap_data(session: pd.DataFrame):
     return
