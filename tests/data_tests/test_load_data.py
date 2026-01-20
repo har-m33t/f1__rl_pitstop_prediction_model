41e64140f1a5cdde325c6c669f4f4c364bf511f9
Harmeet Singh <harmees2@uci.edu>
2026-01-20 01:58:10 -0800

    feat(test_load_data): add necessary modules

--- tests/data_tests/test_load_data.py
+++ tests/data_tests/test_load_data.py
@@ -1,15 +1,26 @@
 import unittest
+from unittest.mock import patch, MagicMock
+import pandas as pd
+
+from src.data.load_data import load_event, load_session, load_lap_data
+
+EVENT = "French Grand Prix"
+YEAR = 2021
+
 
 class TestLoadEvent(unittest.TestCase):
-    def test_load_event_regular():
-        return
+    def test_load_event_regular(self):
+        event = load_event(EVENT, YEAR)
+
+        self.assertIsInstance(event, pd.DataFrame)
+        self.assertEqual(event['EventName'], 'French Grand Prix')
     
     def test_load_event_doesnt_exist():
         return 
     
     def test_load_event_improper_type():
         return
-    pass
+
     
 class TestLoadSession(unittest.TestCase):
     def test_load_session_regular():
