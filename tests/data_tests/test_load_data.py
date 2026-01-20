fb3583752b2e8235d6a75f9e512982336f52ac32
Harmeet Singh <harmees2@uci.edu>
2026-01-20 01:10:33 -0800

    feat(test_load_data): create testing module for load_data.py

---
+++ tests/data_tests/test_load_data.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestLoadEvent(unittest.TestCase): 
+    pass
+    
+class TestLoadSession(unittest.TestCase):
+    pass
+
+class TestLoadLapData(unittest.TestCase):
+    pass
