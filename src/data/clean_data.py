286d823eb6052d7aea386356c3f8cb21e27c128a
Harmeet Singh <harmees2@uci.edu>
2026-01-22 00:14:48 -0800

    feat(clean_data): write skeleton code for data cleaning module

---
+++ src/data/clean_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def drop_columns():
+    return 
+
+def change_time_to_seconds():
+    return 
+
+def handle_missing_values():
+    return 
+
+def filter_invalid_laps():
+    return
+
+def clean_data(df: pd.DataFrame):
+    return df
